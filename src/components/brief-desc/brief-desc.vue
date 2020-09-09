b28e5f8cfaa1b01c0017eddabdd9bf08ede7e567
steven <steven@nenyah.com>
2020-09-09 23:05:14 +0800

    feat(brief-desc.vue): add style

--- src/components/brief-desc/brief-desc.vue
+++ src/components/brief-desc/brief-desc.vue
@@ -6,37 +6,31 @@
  * @LastEditTime: 2020-09-08 14:32:04
 -->
 <template>
-	<view>
-		<vote-rule></vote-rule>
-		<view>
-			<view class="attr">活动详情：</view>
-			<view class="value">展开></view>
-		</view>
-
-	</view>
+  <view>
+    <vote-rule></vote-rule>
+    <view class="text-white flex px-2">
+      <view class="attr"
+        ><view class="fa fa-clock-o text-orange-500 mr-2"></view
+        >活动详情：</view
+      >
+      <view class="flex">展开<view class="text-orange-500 ml-2">></view></view>
+    </view>
+  </view>
 </template>
 
 <script lang="ts">
-	import Vue from 'vue';
-	import voteRule from "@/components/vote-rule/vote-rule.vue";
-	export default Vue.extend({
-		data() {
-			return {
-				
-			}
-		},
-		onLoad() {
-	
-		},
-		methods: {
-	
-		},
-		components:{
-			voteRule
-		}
-	});
+import Vue from "vue"
+import voteRule from "@/components/vote-rule/vote-rule.vue"
+export default Vue.extend({
+  data() {
+    return {}
+  },
+  onLoad() {},
+  methods: {},
+  components: {
+    voteRule,
+  },
+})
 </script>
 
-<style>
-
-</style>
+<style></style>
