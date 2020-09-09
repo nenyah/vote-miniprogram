8cb47e2e0ea84fbd965490a79c1214dc7beaaac0
steven <steven@nenyah.com>
2020-09-09 23:29:18 +0800

    feat(search-bar.vue): add style

--- src/components/search-bar/search-bar.vue
+++ src/components/search-bar/search-bar.vue
@@ -1,26 +1,24 @@
 <template>
-	<view>
-		<input type="text" value="搜索编号/参与项目" />
-	</view>
+  <view class="flex bg-white opacity-25 p-2 my-2">
+    <input
+      class="border-b border border-yellow-500 px-4 block w-full "
+      type="text"
+      placeholder="搜索编号/参与项目"
+      placeholder-class="text-gray-900"
+    />
+    <view class="fa fa-search text-orange-500"></view>
+  </view>
 </template>
 
 <script lang="ts">
-	import Vue from 'vue';
-	export default Vue.extend({
-		data() {
-			return {
-				
-			}
-		},
-		onLoad() {
-	
-		},
-		methods: {
-	
-		},
-	});
+import Vue from "vue"
+export default Vue.extend({
+  data() {
+    return {}
+  },
+  onLoad() {},
+  methods: {},
+})
 </script>
 
-<style>
-
-</style>
+<style></style>
