f5d89b09cc1374bd8c104765aa79b954d587bbfd
nenyah <steven@nenyah.com>
2020-09-09 16:54:35 +0800

    feat(stats.vue): add border

--- src/components/stats/stats.vue
+++ src/components/stats/stats.vue
@@ -3,18 +3,18 @@
  * @Author: Steven
  * @Date: 2020-09-08 09:22:09
  * @LastEditors: Steven
- * @LastEditTime: 2020-09-09 16:45:26
+ * @LastEditTime: 2020-09-09 16:52:17
 -->
 <template>
-  <view>
+  <view class="px-4 pt-2">
     <title></title>
-    <view>
-      <view>
+    <view class="w-4-5 border-2 border-gray-100">
+      <view class="text-yellow-900">
         <view>100</view>
         <view>1678395</view>
         <view>45597802</view>
       </view>
-      <view>
+      <view class="text-gray-100">
         <view>参赛选手</view>
         <view>累计票数</view>
         <view>累计访问</view>
