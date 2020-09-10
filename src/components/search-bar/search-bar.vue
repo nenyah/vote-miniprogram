38d7859a77eefc4a96d8430cfa886aab4fcf2d1b
nenyah <steven@nenyah.com>
2020-09-10 09:12:35 +0800

    feat(search-bar.vue): modify style

--- src/components/search-bar/search-bar.vue
+++ src/components/search-bar/search-bar.vue
@@ -1,12 +1,23 @@
+<!--
+ * @Description: 
+ * @Author: Steven
+ * @Date: 2020-09-08 09:23:10
+ * @LastEditors: Steven
+ * @LastEditTime: 2020-09-10 09:10:02
+-->
 <template>
   <view class="flex bg-white opacity-25 p-2 my-2">
-    <input
-      class="border-b border border-yellow-500 px-4 block w-full "
-      type="text"
-      placeholder="搜索编号/参与项目"
-      placeholder-class="text-gray-900"
-    />
-    <view class="fa fa-search text-orange-500"></view>
+    <view
+      class="flex w-full py-2 border-b-2 border-r-0 border-l-0 border-t-0 border-solid border-gray-200 "
+    >
+      <input
+        class="px-4 block w-full"
+        type="text"
+        placeholder="搜索编号/参与项目"
+        placeholder-class="text-gray-900"
+      />
+      <view class="fa fa-search text-orange-500"></view>
+    </view>
   </view>
 </template>
 
