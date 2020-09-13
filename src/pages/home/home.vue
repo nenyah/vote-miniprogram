b84ee364420eb6dc4a2c20a9d6f4f072aa68d0cd
steven <steven@nenyah.com>
2020-09-13 15:31:03 +0800

    fix(*): test uni api

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -5,7 +5,29 @@
 <script lang="ts">
 import Vue from "vue"
 import {} from "@/mock/store"
-export default Vue.extend({})
+export default Vue.extend({
+  onLoad() {
+    uni.authorize({
+      scope: "scope.userInfo",
+      success: () => {
+        uni.login({
+          provider: "weixin",
+          success: (loginRes) => {
+            console.log("loginRes", loginRes)
+            // 获取用户信息
+            uni.getUserInfo({
+              provider: "weixin",
+              lang: "zh_CN",
+              success: (infoRes) => {
+                console.log("用户信息为：", infoRes.userInfo)
+              },
+            })
+          },
+        })
+      },
+    })
+  },
+})
 </script>
 
 <style></style>
