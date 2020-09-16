f801fddcd339f69a1157cba59721c92459cd6759
Codet <lucibriel@163.com>
2020-09-16 23:18:57 +0800

    fix(login): add style

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -1,7 +1,7 @@
 <template>
-  <view>
+  <view class="flex flex-col justify-center items-center content-center h-full">
     婉美投票
-    <button class="" open-type="getUserInfo" @getuserinfo="getuserinfo">
+    <button class="text-gray-100 bg-green-500 p-2 mt-5 w-4--5 rounded-full border-none border-0" open-type="getUserInfo" @getuserinfo="getuserinfo">
       授权使用微信头像、昵称
     </button>
   </view>
@@ -14,12 +14,25 @@ export default Vue.extend({
   methods: {
     getuserinfo() {
       uni.authorize({
-        scope: "scope.userinfo",
-        success: res => {
+        scope: "scope.userInfo",
+        success: (res) => {
           console.log("授权成功", res)
+          //  成功后，获取userinfo
+          uni.getUserInfo({
+            provider: "weixin",
+            success: res => {
+              console.log("成功获取用户信息", res)
+            },
+            fail: err => {
+              console.error("获取用户信息失败", err)
+            }
+          })
+        },
+        fail: err => {
+          console.error("授权失败", err)
         }
       })
-    }
+    },
   },
 })
 </script>
