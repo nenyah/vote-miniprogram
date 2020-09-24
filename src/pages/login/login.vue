1a6c97948dc11a2c5dd4c56a301ab95b3d027af0
steven <steven@nenyah.com>
2020-09-24 17:03:33 +0800

    fix(*): adjust requests add token

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -75,28 +75,28 @@ export default Vue.extend({
                       key: "userInfo",
                       data: userInfoData,
                     })
-                    // try {
-                    //   // 上传用户信息
-                    //   await userInfo({
-                    //     openId: data.openId,
-                    //     signature: res.signature,
-                    //     encryptedData: res.encryptedData,
-                    //     iv: res.iv,
-                    //   })
-                    // } catch (error) {
-                    //   console.error("上传用户信息失败", error)
-                    // }
+                    try {
+                      // 上传用户信息
+                      await userInfo({
+                        openId: data.openId,
+                        signature: res.signature,
+                        encryptedData: res.encryptedData,
+                        iv: res.iv,
+                      })
+                    } catch (error) {
+                      console.error("上传用户信息失败", error)
+                    }
 
                     // 成功后，跳转到活动列表页
-                    // uni.reLaunch({
-                    //   url: "../index/index",
-                    //   success: (res) => {
-                    //     console.log("跳转成功", res)
-                    //   },
-                    //   fail: (err) => {
-                    //     console.log("跳转失败", err)
-                    //   },
-                    // })
+                    uni.reLaunch({
+                      url: "../index/index",
+                      success: (res) => {
+                        console.log("跳转成功", res)
+                      },
+                      fail: (err) => {
+                        console.log("跳转失败", err)
+                      },
+                    })
                   }
                 }
                 ,
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -3,43 +3,42 @@
  * @Author: Steven
  * @Date: 2020-09-11 08:52:11
  * @LastEditors: Steven
- * @LastEditTime: 2020-09-24 15:34:54
+ * @LastEditTime: 2020-09-24 16:56:45
  */
-import {IglobalData} from "@/common/interface"
+import { IglobalData } from "@/common/interface"
 
 interface IParams {
-    url: string
-    method?: "GET" | "POST" | "PUT"
-    setUpUrl?: boolean
-    data?: any
+  url: string
+  method?: "GET" | "POST" | "PUT"
+  setUpUrl?: boolean
+  data?: any
 }
 
-// let {token} = getApp().globalData as IglobalData
 export default (params: IParams) => {
-    // 加载中
-    uni.showLoading({
-        title: "加载中",
-    })
-    return new Promise(
-        (resolve: (value: any) => void, reject: (value: any) => void) => {
-            console.log(`正在请求：${params.url}`)
-            let defaultParams = {
-                timeout: 6000,
-                ...params,
-            }
-            uni.request({
-                ...defaultParams,
-                // header: {Authorization: token || undefined},
-                success(res) {
-                    resolve(res)
-                },
-                fail(err) {
-                    reject(err)
-                },
-                complete() {
-                    uni.hideLoading()
-                },
-            })
-        }
-    )
+  // 加载中
+  uni.showLoading({
+    title: "加载中",
+  })
+  return new Promise(
+    (resolve: (value: any) => void, reject: (value: any) => void) => {
+      console.log(`正在请求：${params.url}`)
+      let defaultParams = {
+        timeout: 6000,
+        ...params,
+      }
+      uni.request({
+        ...defaultParams,
+        header: { Authorization: (getApp().globalData as IglobalData).token || undefined },
+        success(res) {
+          resolve(res)
+        },
+        fail(err) {
+          reject(err)
+        },
+        complete() {
+          uni.hideLoading()
+        },
+      })
+    }
+  )
 }
