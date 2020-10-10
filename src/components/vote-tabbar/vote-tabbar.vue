2c182687f11cfd5a5f3e5b6dd582af335f80acd6
steven <steven@nenyah.com>
2020-10-10 17:04:07 +0800

    fix(*): add icon

--- src/components/vote-tabbar/vote-tabbar.vue
+++ src/components/vote-tabbar/vote-tabbar.vue
@@ -1,15 +1,16 @@
 <template>
-  <view style="background-color:#ec7db0; width: 750rpx;" class="flex items-center h-12 absolute bottom-0 left-0 text-gray-500">
+  <view style="background-color:#ec7db0; width: 750rpx;"
+        class="flex items-center h-12 absolute bottom-0 left-0 text-gray-500">
     <view class="flex-1 text-center box-border py-1">
-      <view></view>
+      <view class="fa fa-fire"></view>
       <view>投票</view>
     </view>
     <view class="flex-1 text-center box-border py-1">
-      <view></view>
+      <view class="fa fa-info-circle"></view>
       <view>详情</view>
     </view>
     <view class="flex-1 text-center box-border py-1">
-      <view class="fa fa-random"></view>
+      <view class="fa fa-bar-chart"></view>
       <view>排名</view>
     </view>
   </view>
